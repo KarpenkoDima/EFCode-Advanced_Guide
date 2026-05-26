82117ecfd725cea4150ac480f64c05873d760c0f
Dima Karpenko dimak <dimka2010@gmail.com>
2026-05-26 09:45:15 +0300

    2.4 Базовый класс для агрегатов

---
+++ src/Dispancer.Domain/Entities/AggregateRoot.cs
@@ -0,0 +1,39 @@
+﻿using Dispancer.Domain.Interfaces;
+
+namespace Dispancer.Domain.Entities;
+
+public abstract class AggregateRoot: IHasDomainEvents, IAuditable, ISoftDeletable
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    // IHasDomainEvents
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    public void ClearDomainEvents() => _domainEvents.Clear();
+    
+    protected void RaiseDomainEvent(IDomainEvent domainEvent) =>
+        _domainEvents.Add(domainEvent);
+    
+    // IAuditable
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? ModifiedAt { get; private set; }
+
+    public void TouchModifiedAt() => ModifiedAt = DateTime.UtcNow;
+
+    // ISoftDeletable
+    public bool IsDeleted { get; private set; }
+    public DateTime? DeletedAt { get; private set; }
+    
+    // ISoftDeletable - вызывается SoftDeleteInterceptor
+    public void MarkAsDeleted()
+    {
+        IsDeleted = true;
+        DeletedAt = DateTime.UtcNow;
+    }
+    
+    // Вызывается только при создании через фабричный метод
+    protected void InitAudit()
+    {
+        CreatedAt = DateTime.UtcNow;
+        ModifiedAt = DateTime.UtcNow;
+    }
+}
